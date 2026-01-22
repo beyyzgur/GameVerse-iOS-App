957db83ed6ad4d928c6c935df3d7f59cf7151a9d
Ayşe Beyza Gür <beyyzgur@gmail.com>
2026-01-23 02:35:34 +0300

    refactor: update AlertPresentable protocol

--- GameVerseApp/GameVerseApp/GeneralProtocols/AlertPresentable.swift
+++ GameVerseApp/GameVerseApp/GeneralProtocols/AlertPresentable.swift
@@ -7,11 +7,15 @@
 import UIKit
 
 protocol AlertPresentable {
-    func makeAlert(title: String, message: String, onOK: (() -> Void)?)
+    func makeAlert(title: String,
+                   message: String,
+                   onOK: (() -> Void)?)
 }
 
 extension AlertPresentable where Self: UIViewController {
-    func makeAlert(title: String, message: String, onOK: (() -> Void)? = nil) {
+    func makeAlert(title: String,
+                   message: String,
+                   onOK: (() -> Void)? = nil) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
         let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) { _ in
             onOK?()
