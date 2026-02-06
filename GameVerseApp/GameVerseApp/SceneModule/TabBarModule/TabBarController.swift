f4dc62de93b05157d2ec7e9ba5b1b815755a444d
Ayşe Beyza Gür <beyyzgur@gmail.com>
2026-02-06 12:05:29 +0300

    feat: update TabBar Module

--- GameVerseApp/GameVerseApp/SceneModule/TabBarModule/TabBarController.swift
+++ GameVerseApp/GameVerseApp/SceneModule/TabBarModule/TabBarController.swift
@@ -17,7 +17,6 @@ final class TabBarController: UITabBarController {
     let homeStoryBoard = UIStoryboard(name: StoryBoardName.home.rawValue, bundle: nil)
     let favoritesStoryBoard = UIStoryboard(name: StoryBoardName.favorites.rawValue, bundle: nil)
     let discoverStoryBoard = UIStoryboard(name: StoryBoardName.discover.rawValue, bundle: nil)
-    let profileStoryBoard = UIStoryboard(name: StoryBoardName.profile.rawValue, bundle: nil)
     
     func setupControllers() {
         // MARK: - home
@@ -28,6 +27,15 @@ final class TabBarController: UITabBarController {
         homeNavController.tabBarItem = UITabBarItem(title: StoryBoardName.home.rawValue,
                                                     image: UIImage(systemName: "house"),
                                                     tag: 0)
+        
+        // MARK: - discover
+        guard let discoverViewController = discoverStoryBoard.instantiateViewController(withIdentifier:
+                                                                                            StoryboardIdentifier.discover.rawValue) as? DiscoverViewController
+          else { return }
+        let discoverNavController = UINavigationController(rootViewController: discoverViewController)
+        discoverNavController.tabBarItem = UITabBarItem(title: StoryBoardName.discover.rawValue,
+                                                        image: UIImage(systemName: "gamecontroller"),
+                                                        tag: 1)
         // MARK: - favorites
         
         guard let favoritesViewController = favoritesStoryBoard.instantiateViewController(withIdentifier:
@@ -36,30 +44,14 @@ final class TabBarController: UITabBarController {
         let favoritesNavController = UINavigationController(rootViewController: favoritesViewController)
         favoritesNavController.tabBarItem = UITabBarItem(title: "Wishlist",
                                                          image: UIImage(systemName: "star"),
-                                                         tag: 1)
-        // MARK: - discover
-        guard let discoverViewController = discoverStoryBoard.instantiateViewController(withIdentifier:
-                                                                                            StoryboardIdentifier.discover.rawValue) as? DiscoverViewController
-          else { return }
-        let discoverNavController = UINavigationController(rootViewController: discoverViewController)
-        discoverNavController.tabBarItem = UITabBarItem(title: StoryBoardName.discover.rawValue,
-                                                        image: UIImage(systemName: "gamecontroller"),
-                                                        tag: 2)
-        // MARK: - profile
-        guard let profileViewController = profileStoryBoard.instantiateViewController(withIdentifier:
-                                                                                        StoryboardIdentifier.profile.rawValue) as? ProfileViewController
-          else { return }
-        let profileNavController = UINavigationController(rootViewController: profileViewController)
-        profileNavController.tabBarItem = UITabBarItem(title: StoryBoardName.profile.rawValue,
-                                                       image: UIImage(systemName: "person"),
-                                                       tag: 3)
-        
+                                                         tag: 2)
+      
         
         setViewControllers([
             homeNavController,
-            favoritesNavController,
             discoverNavController,
-            profileNavController],
+            favoritesNavController
+            ],
                            animated: true)
         
     }
